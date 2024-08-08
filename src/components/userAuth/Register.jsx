50c1e8a07859ac2761db76bcb1626ca0c403aea5
Yassine Dorgâa <58447334+YassineDG@users.noreply.github.com>
2024-08-08 18:23:53 +0100

    Update Register.jsx

--- src/components/userAuth/Register.jsx
+++ src/components/userAuth/Register.jsx
@@ -15,7 +15,7 @@ export default function Register() {
   const registerUser = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://vr-backend-production-9494.up.railway.app/api/users/register', {
+      const response = await axios.post('https://vr-backend-6e7h.onrender.com/api/users/register', {
         email,
         password
       });
